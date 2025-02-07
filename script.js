2b80c98d087288f3439eb73f0cd5033d3207be38
Nono13122002 <nolwenn.lerohellec@gmail.com>
2025-02-07 19:21:20 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector("#a-propos").addEventListener("click", function() {
+        alert("Bienvenue sur mon CV en ligne !");
+    });
+});
